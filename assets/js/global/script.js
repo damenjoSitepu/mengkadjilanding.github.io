abce2f168ed632939d3cbb37fb8ac43c5d0620c6
damenjoSitepu <damenstory@gmail.com>
2021-10-22 20:58:58 +0700

    Final Battle 1

--- assets/js/global/script.js
+++ assets/js/global/script.js
@@ -1,3 +1,82 @@
+$(window).on('load', function () {
+    // Scroll Reveal
+    ScrollReveal({
+        reset: false,
+        distance: '60px',
+        duration: 2500,
+        delay: 500
+    })
+
+    // Starter
+    ScrollReveal().reveal('.m-program-categorys', {
+        origin: 'top'
+    });
+
+    ScrollReveal().reveal('.m-program-category-spawn', {
+        delay: 250,
+        origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.m-program-category-container', {
+        delay: 250,
+        origin: 'top'
+    });
+
+
+    // review
+    ScrollReveal().reveal('.m-review h2', {
+        delay: 250,
+        origin: 'top'
+    });
+
+    ScrollReveal().reveal('.m-review h3', {
+        delay: 250,
+        origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.rcv-1', {
+        delay: 250,
+        origin: 'top'
+    });
+
+    ScrollReveal().reveal('.rcv-2', {
+        delay: 250,
+        origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.m-review-content-mid', {
+        delay: 250,
+        origin: 'top'
+    });
+
+
+    // Interested
+    ScrollReveal().reveal('.m-interested-content-lists-1', {
+        delay: 250,
+        origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.m-interested-content-lists-2', {
+        delay: 250,
+        origin: 'top'
+    });
+
+    ScrollReveal().reveal('.m-interested-content-lists-3', {
+        delay: 250,
+        origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.m-interested-content-sticky', {
+        delay: 250,
+        origin: 'top'
+    });
+
+    // Social Media
+    ScrollReveal().reveal('.m-social-media', {
+        delay: 250
+    });
+});
+
 $(document).ready(function () {
     setTimeout(function () {
         $(".loadSaber").hide();
@@ -191,85 +270,6 @@ $(document).ready(function () {
         }
     }
 
-    // Scroll Reveal
-    ScrollReveal({
-        reset: false,
-        distance: '60px',
-        duration: 2500,
-        delay: 500
-    })
-
-    // Starter
-    ScrollReveal().reveal('.m-program-categorys', {
-        origin: 'top'
-    });
-
-    ScrollReveal().reveal('.m-program-category-spawn', {
-        delay: 250,
-        origin: 'bottom'
-    });
-
-
-
-
-
-
-
-
-
-
-    // review
-    ScrollReveal().reveal('.m-review h2', {
-        delay: 250,
-        origin: 'top'
-    });
-
-    ScrollReveal().reveal('.m-review h3', {
-        delay: 250,
-        origin: 'bottom'
-    });
-
-    ScrollReveal().reveal('.rcv-1', {
-        delay: 250,
-        origin: 'top'
-    });
-
-    ScrollReveal().reveal('.rcv-2', {
-        delay: 250,
-        origin: 'bottom'
-    });
-
-    ScrollReveal().reveal('.m-review-content-mid', {
-        delay: 250,
-        origin: 'top'
-    });
-
-
-    // Interested
-    ScrollReveal().reveal('.m-interested-content-lists-1', {
-        delay: 250,
-        origin: 'bottom'
-    });
-
-    ScrollReveal().reveal('.m-interested-content-lists-2', {
-        delay: 250,
-        origin: 'top'
-    });
-
-    ScrollReveal().reveal('.m-interested-content-lists-3', {
-        delay: 250,
-        origin: 'bottom'
-    });
-
-    ScrollReveal().reveal('.m-interested-content-sticky', {
-        delay: 250,
-        origin: 'top'
-    });
-
-    // Social Media
-    ScrollReveal().reveal('.m-social-media', {
-        delay: 250
-    });
 
 
 });
